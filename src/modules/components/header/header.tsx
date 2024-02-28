0ff1ac9b6682443edfc076845498d8a0372b058b
marcoslafoz <marcos.lafoz96@gmail.com>
2024-02-28 18:06:01 +0100

    feat: :heavy_plus_sign: Header added

--- src/modules/components/header/header.tsx
+++ src/modules/components/header/header.tsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import './header.css'
 
-interface Props {
-  prop1?: string
-}
+interface Props { }
 
 export const Header: React.FC<Props> = props => {
- 
-  //const {prop1} = props
- 
+
   return (
 
-  <header className="headerNav">
-    <div className="topName">marcos<span className="lafozName">lafoz</span></div>
-    <nav className="topNav">
-      <ul>
-        <li><button id="btn-about-me">Inicio</button></li>
-        <li><button id="btn-about-me">Sobre Mí</button></li>
-        <li><button id="btn-about-me">Conocimientos</button></li>
-        <li><button id="btn-about-me">Proyectos</button></li>
+    <header className=" container-fluid d-flex flex-wrap justify-content-center py-4 mb-4">
+
+      <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none ">
+        <span className='text-bold font-size-2 font-color-primary' >marcos</span>
+        <span className='font-size-2 font-color-primary'>lafoz</span>
+      </div>
+
+      <ul className="nav nav-pills">
+        <li className="nav-item"><div className="nav-link font-color-primary text-medium" >Inicio</div></li>
+        <li className="nav-item"><div className="nav-link font-color-primary text-medium">Sobre Mí</div></li>
+        <li className="nav-item"><div className="nav-link font-color-primary text-medium">Conocimientos</div></li>
+        <li className="nav-item"><div className="nav-link font-color-primary text-medium">Proyectos</div></li>
       </ul>
-    </nav>
-  </header>
-  );
+
+    </header>
+
+  )
 }
 
--- src/modules/scenes/main/main.tsx
+++ src/modules/scenes/main/main.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Header } from '../../components';
 import './main.css'
 
-interface MainProps {
-  prop1?: string
-}
+interface MainProps { }
 
 export const Main: React.FC<MainProps> = props => {
  
-  //const {prop1} = props
- 
   return (
-    <Header/>
+    <div className="main-container container-fluid">
+      <Header/>
+    </div>
   )
 }
 
