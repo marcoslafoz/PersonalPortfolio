cf45c90d0a2c30ff504c401e02d595559111f7cf
Marcos Lafoz <marcos.lafoz96@gmail.com>
2024-03-01 19:58:02 +0100

    fix: :wrench: download cv

--- src/modules/components/about-me/download-cv-button/download-cv-button.tsx
+++ src/modules/components/about-me/download-cv-button/download-cv-button.tsx
@@ -8,7 +8,7 @@ export const DownloadCvButton: React.FC<DownloadCvButtonProps> = props => {
   return (
 
     <button className="download-button">
-      <a href="./src/docs/CV_LafozMiranda_Marcos.pdf" target="_blank">
+      <a href="https://drive.google.com/file/d/1PaYKlNzwlSO4oUeLPfd59EJrNZvk_3W0/view?usp=sharing" rel='noreferrer' target="_blank">
         <div className="docs"><svg className="css-i6dzq1" stroke-linejoin="round" stroke-linecap="round" fill="none"
           stroke-width="2" stroke="currentColor" height="20" width="20" viewBox="0 0 24 24">
           <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
