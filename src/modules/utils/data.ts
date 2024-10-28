f8be45df7fa4fdb71de55d0d5fb3399441abe4c8
Marcos Lafoz <marcos.lafoz96@gmail.com>
2024-10-28 17:32:20 +0100

    Update data.ts

--- src/modules/utils/data.ts
+++ src/modules/utils/data.ts
@@ -17,27 +17,6 @@ export const marcosProjectData: ProjectType[] = [
     website: 'https://meme-generator.lafoz.dev/',
     github: 'https://github.com/marcoslafoz/meme-generator'
   },
-  {
-    previewImg: vintedBotImg,
-    name: 'VINTED-BOT (2023)',
-    description: 'This project is a Vinted bot that enhances product visibility and sales success by adding custom visits. It utilizes JavaScript and Node.js for implementation.',
-    website: 'https://github.com/marcoslafoz/VintedBot/blob/master/README.md',
-    github: 'https://github.com/marcoslafoz/VintedBot'
-  },
-  {
-    previewImg: zgzAppImg,
-    name: 'ZGZAPP (2023)',
-    description: 'This page is a web application project that offers Zaragoza city services. (unfinished)',
-    website: 'https://zgzapp.lafoz.dev/',
-    github: 'https://github.com/marcoslafoz/zgzApp'
-  },
-  {
-    previewImg: fasfuImg,
-    name: 'FASFÚ BURGUERS (2022)',
-    description: 'This website is a simple project from January 2022 about a fast-food restaurant called Fasfu.',
-    website: 'https://fasfu.lafoz.dev/',
-    github: 'https://github.com/marcoslafoz/Fasfu'
-  }
 ]
 
 export const marcosSkillsData: SkillsCategoryType[] = [
